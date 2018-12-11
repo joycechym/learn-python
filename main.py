e933395f4a8791a6c3cde4264639fb4ce7fed4bd
joycechym <45790655+joycechym@users.noreply.github.com>
2018-12-12 03:09:42 +0800

    Uploaded main.py.
    Codes for learning python and keep them in python

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello World")
+intvar=2
+floatvar=1.4
+strvar="Hello World"
+charvar='c'
+print("intvar is ",intvar)
+print("floatvar is ",floatvar)
+print("strvar is ",strvar)
+print("charvar is ",charvar)
+
+#this is a single line comment
+""" multiple line comment
+therefore write whatever you want therefore 
+and no offense """
