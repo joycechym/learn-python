f359a72430a79e5e4dd9ff29b1fa6c379e0433a1
joycechym <45790655+joycechym@users.noreply.github.com>
2018-12-12 03:30:17 +0800

    Latest main.py

--- main.py
+++ main.py
@@ -20,3 +20,38 @@ print("charvar is ",charvar)
 """ multiple line comment
 therefore write whatever you want therefore 
 and no offense """
+
+#Data structures in python are list, tuples, set and dictionary
+#create a list
+mylist=[]
+mylist.append(2)
+mylist.append(1.77)
+mylist.append("Joyce's String in the list")
+mylist.append('k')
+print(mylist)
+
+#Get info from input
+name=input("Please enter your name: ")
+print("Hello ",name)
+
+num1=int(input("Enter num1 : "))
+num2=int(input("Enter num2 : "))
+result=num1*num2
+print("Multiple result of ",num1,"*",num2," is ",num1*num2)
+
+#python selection statement
+if(num1>10):
+    print("Number is more than 10")
+elif(num1<10 and num1>1):
+    print("Number is between 1 and 10")
+else:
+    print("Number is less than or equal 1")
+    
+def hellofunc():
+    print("hellofunc here")
+    msg=input("enter a hello message : ")
+    print(msg)
+
+#Now call the function
+hellofunc()
+
